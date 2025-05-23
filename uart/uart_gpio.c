c5feb7176e1868b976a157e309712a3086c84a82
mock <lunarbuffalomoon@gmail.com>
2025-05-22 21:35:24 -0400

    starting uart project

---
+++ uart/uart_gpio.c
@@ -0,0 +1,10 @@
+#include <inttype.h>
+
+struct uart
+{
+	volatile uint32_t SR, DR, BRR, CR1, CR2, CR3, GTPR;
+};
+#define UART1 ((struct uart *) 0x40011000)
+#define UART2 ((struct uart *) 0x40004400)
+#define UART3 ((struct uart *) 0x40004800)
+
